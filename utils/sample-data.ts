f9156ddc57893923f68ffdd38c5b9d39e75d6845
Johan Thornström <johan.o.thornstrom@gmail.com>
2023-01-02 18:09:44 +0100

    dumb test

--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -2,8 +2,8 @@ import { Product } from '../interfaces'
 
 /** Dummy item data. */
 export const sampleFruitsData: Product[] = [
-  { id: 101, name: 'Apple', price: 10, imgUrl: '/images/apple.png' },
-  { id: 102, name: 'Banana', price: 20, imgUrl: '/images/banana.png' },
-  { id: 103, name: 'Orange', price: 30, imgUrl: '/images/orange.png' },
-  { id: 104, name: 'Pear', price: 40, imgUrl: '/images/pear.png' },
+  { id: 101, name: 'Apple', price: 10, imgUrl: 'products/apple.png' },
+  { id: 102, name: 'Banana', price: 20, imgUrl: 'products/banana.png' },
+  { id: 103, name: 'Orange', price: 30, imgUrl: 'products/orange.png' },
+  { id: 104, name: 'Pear', price: 40, imgUrl: 'products/pear.png' },
 ]
